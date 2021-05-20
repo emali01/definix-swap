6dd7e5986fb1757c466c3edd8f99f34086a7f7d1
Tanapat Sainak <fallen_things@hotmail.com>
2021-05-20 15:34:35 +0700

    save

--- src/constants/token/definixswap.ts
+++ src/constants/token/definixswap.ts
@@ -13,10 +13,18 @@ export default {
     {
       name: 'GAS Token',
       symbol: 'GAS',
-      address: '0x4f9a66D44bdEebf25109257985fFBaDacc5a7317',
-      chainId: 8217,
+      address: '0x03CB8549cBCc01f90A8A699F3e069ebb971C9B8C',
+      chainId: 1001,
       decimals: 18,
       logoURI: '/images/coins/GAS.png'
+    },
+    {
+      name: 'MUAY Token',
+      symbol: 'MUAY',
+      address: '0x4466E4922aB640CF84Cd87001fCc7B627b896ea6',
+      chainId: 1001,
+      decimals: 18,
+      logoURI: '/images/coins/MUAY.png'
     },
   ]
 }
