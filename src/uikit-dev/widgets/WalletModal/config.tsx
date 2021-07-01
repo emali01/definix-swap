1b636a83a11ac1ef8dac98e6615fea042a55f271
NANTANART TAKONUAN <nantanart@six.network>
2021-07-01 22:52:56 +0700

    Add DCent wallet connect

--- src/uikit-dev/widgets/WalletModal/config.tsx
+++ src/uikit-dev/widgets/WalletModal/config.tsx
@@ -1,16 +1,16 @@
-import Metamask from './icons/Metamask'
-import MathWallet from './icons/MathWallet'
-import TokenPocket from './icons/TokenPocket'
-import TrustWallet from './icons/TrustWallet'
-import WalletConnect from './icons/WalletConnect'
-import BinanceChain from './icons/BinanceChain'
 import Kaikas from './icons/Kaikas'
+import Dcent from './icons/Dcent'
 import { Config } from './types'
 
 const connectors: Config[] = [
   {
     title: 'Kaikas',
     icon: Kaikas,
+    connectorId: 'injected',
+  },
+  {
+    title: 'D`CENT',
+    icon: Dcent,
     connectorId: 'injected',
   },
 ]
