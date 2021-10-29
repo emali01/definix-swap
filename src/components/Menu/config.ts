e61f06542f1428a25f77d24fff991f82b06b5568
Nutnaree <nutnaree@six.network>
2021-10-29 13:13:41 +0700

    Add Menu Long-term-stake on klaytn

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -16,6 +16,8 @@ import partnerBlack from 'uikit-dev/images/for-ui-v2/menu/Partnership-Black.png'
 import partnerWhite from 'uikit-dev/images/for-ui-v2/menu/Partnership-White.png'
 import swapBlack from 'uikit-dev/images/for-ui-v2/menu/Swap-Black.png'
 import swapWhite from 'uikit-dev/images/for-ui-v2/menu/Swap-White.png'
+import safeBlack from 'uikit-dev/images/for-ui-v2/menu/Safe-Black.png'
+import safeWhite from 'uikit-dev/images/for-ui-v2/menu/Safe-White.png'
 
 const config: MenuEntry[] = [
   {
@@ -90,6 +92,22 @@ const config: MenuEntry[] = [
     href: `${process.env.REACT_APP_FRONTEND_URL}/rebalancing`,
     notHighlight: false,
     newTab: false,
+  },
+  {
+    label: 'Finix',
+    icon: safeBlack,
+    iconActive: safeWhite,
+    href: '/finix',
+    notHighlight: false,
+    newTab: false,
+    items: [
+      {
+        label: 'Long-term Stake',
+        href: `${process.env.REACT_APP_FRONTEND_URL}/long-term-stake`,
+        notHighlight: false,
+        newTab: false,
+      },
+    ],
   },
   // {
   //   label: 'Vault',
