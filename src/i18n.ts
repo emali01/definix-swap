31b63771cb1cb61b4dbd89c9ede7c5883295d81c
Jiyoung Kim <jyk@mhand.net>
2021-12-16 19:27:28 +0900

    feat: Remove i18next-http-backend

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,18 +1,32 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 
-import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
+// don't want to use this?
+// have a look at the Quick start guide
+// for passing in lng and translations on init
+import enJSON from './locales/en.json'
+import koJSON from './locales/ko.json'
+
+const resources = {
+  en: {
+    translation: enJSON,
+  },
+  ko: {
+    translation: koJSON,
+  },
+}
 
 i18n
-  .use(Backend)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
   .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    backend: {
-      loadPath: `./locales/{{lng}}.json`,
-    },
-    load: 'languageOnly',
+    // debug: true,
     react: {
       useSuspense: true,
     },
@@ -20,12 +34,12 @@ i18n
     //   order: ['localStorage', 'querystring', 'navigator'],
     //   lookupQuerystring: 'lng',
     // },
+    resources,
     fallbackLng: 'en',
     supportedLngs: ['en', 'ko'],
-    preload: ['en'],
     keySeparator: false,
     interpolation: {
-      escapeValue: false
+      escapeValue: false, // not needed for react as it escapes by default
     },
   })
 
