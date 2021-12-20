b57ed5a5236bdb8991e39ecd8072c6071dddd4f6
GeonHyeok-Lee <geonhyeoklee.kr@gmail.com>
2021-12-20 22:54:06 +0900

    fix: Change placeholder and style [GZ2021-421]

--- src/components/CurrencyInputPanel/index.tsx
+++ src/components/CurrencyInputPanel/index.tsx
@@ -1,9 +1,9 @@
 import { Currency, Pair } from 'definixswap-sdk'
 import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import BigNumber from "bignumber.js";
+import BigNumber from 'bignumber.js'
 import styled from 'styled-components'
-import { 
+import {
   Text,
   Flex,
   Box,
@@ -25,7 +25,7 @@ import { Input as NumericalInput } from '../NumericalInput'
 import CurrencySearchModal from '../SearchModal/CurrencySearchModal'
 
 interface CurrencyInputPanelProps {
-  isMobile: boolean;
+  isMobile: boolean
   value: string
   currency?: Currency | null
   disableCurrencySelect?: boolean
@@ -43,10 +43,6 @@ interface CurrencyInputPanelProps {
   maxTokenAmount?: string
 }
 
-const InputBox = styled.div`
-  display: flex;
-  justify-content: space-between;
-`
 const CurrencySelect = styled.button<{ selected: boolean }>`
   padding: 0;
   align-items: center;
@@ -62,7 +58,7 @@ const Input = styled.input`
   outline: none;
   ${textStyle.R_14M}
   ${ColorStyles.BLACK}
-`;
+`
 
 const inputRegex = RegExp(`^\\d*(?:\\\\[.])?\\d*$`) // match escaped "." characters via in a non-capturing group
 export const CurrencyInputPanelOnRemoveLP: React.FC<any> = ({
@@ -73,42 +69,48 @@ export const CurrencyInputPanelOnRemoveLP: React.FC<any> = ({
   value,
   currency,
   currencyA,
-  currencyB
+  currencyB,
 }) => {
   const { isXl, isXxl } = useMatchBreakpoints()
   const isMobile = useMemo(() => !isXl && !isXxl, [isXl, isXxl])
-  const { t } = useTranslation();
-  const enforcer = (nextUserInput: string) => {
-    if (nextUserInput === '' || inputRegex.test(escapeRegExp(nextUserInput))) {
-      onUserInput(nextUserInput)
-    }
-  };
+  const { t } = useTranslation()
+  const enforcer = useCallback(
+    (nextUserInput: string) => {
+      if (nextUserInput === '' || inputRegex.test(escapeRegExp(nextUserInput))) {
+        onUserInput(nextUserInput)
+      }
+    },
+    [onUserInput]
+  )
 
-  const decimals = useMemo(() => 18, []);
+  const onChangeInput = useCallback(
+    (event) => {
+      enforcer(event.target.value.replace(/,/g, '.'))
+    },
+    [enforcer]
+  )
+
+  const decimals = useMemo(() => 18, [])
   const overDp = useMemo(() => new BigNumber(value).decimalPlaces() > decimals, [value, decimals])
-  
+
   const renderNoti = useCallback(() => {
-    if(overDp){
+    if (overDp) {
       return (
         <Noti type={NotiType.ALERT} mt="12px">
           {t('The value entered is out of the valid range')}
         </Noti>
       )
     }
-    return null;
-  }, [t, overDp]);
-
+    return null
+  }, [t, overDp])
 
   return (
     <Flex flexDirection="column">
-      <Flex alignItems="center" mb={isMobile ? "8px" : "16px"}>
+      <Flex alignItems="center" mb={isMobile ? '8px' : '16px'}>
         {currencyA && currencyB && (
           <>
-            <Box mr={isMobile ? "12px" : "10px"}>
-              <Lp 
-                lpSymbols={[currencyA?.symbol, currencyB?.symbol]}
-                size="32px"
-              />
+            <Box mr={isMobile ? '12px' : '10px'}>
+              <Lp lpSymbols={[currencyA?.symbol, currencyB?.symbol]} size="32px" />
             </Box>
             <Text textStyle="R_16M" color={ColorStyles.DEEPGREY}>
               {currencyA?.symbol}-{currencyB?.symbol}
@@ -117,16 +119,10 @@ export const CurrencyInputPanelOnRemoveLP: React.FC<any> = ({
         )}
         {currency && (
           <>
-            <Box mr={isMobile ? "12px" : "10px"}>
-              <Coin 
-                size="32px"
-                symbol={currency?.symbol}
-              />
+            <Box mr={isMobile ? '12px' : '10px'}>
+              <Coin size="32px" symbol={currency?.symbol} />
             </Box>
-            <Text
-              textStyle="R_16M"
-              color={ColorStyles.DEEPGREY}
-            >
+            <Text textStyle="R_16M" color={ColorStyles.DEEPGREY}>
               {currency?.symbol}
             </Text>
           </>
@@ -140,40 +136,22 @@ export const CurrencyInputPanelOnRemoveLP: React.FC<any> = ({
         justifyContent="space-between"
         alignItems="center"
       >
-        <Input
-          placeholder="0.0"
-          value={value}
-          onChange={(event) => {
-            // replace commas with periods, because uniswap exclusively uses period as the decimal separator
-            enforcer(event.target.value.replace(/,/g, '.'))
-          }}
-        />
+        <Input placeholder="0.0" value={value} onChange={onChangeInput} />
         <Flex>
-          <AnountButton 
-            onClick={onQuarter} 
-            mr="6px" 
-            backgroundColor="rgba(224, 224, 224, 0.3)"
-          >
+          <AnountButton onClick={onQuarter} mr="6px" backgroundColor="rgba(224, 224, 224, 0.3)">
             {t('25%')}
           </AnountButton>
-          <AnountButton 
-            onClick={onHalf}
-            mr="6px"
-            backgroundColor="rgba(224, 224, 224, 0.3)"
-          >
+          <AnountButton onClick={onHalf} mr="6px" backgroundColor="rgba(224, 224, 224, 0.3)">
             {t('50%')}
           </AnountButton>
-          <AnountButton 
-            onClick={onMax}
-            backgroundColor="rgba(224, 224, 224, 0.3)"
-          >
+          <AnountButton onClick={onMax} backgroundColor="rgba(224, 224, 224, 0.3)">
             {t('MAX')}
           </AnountButton>
         </Flex>
       </Flex>
       {renderNoti()}
     </Flex>
-  );
+  )
 }
 
 export default function CurrencyInputPanel({
@@ -194,91 +172,85 @@ export default function CurrencyInputPanel({
   isInsufficientBalance,
   maxTokenAmount,
 }: CurrencyInputPanelProps) {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
   const { account } = useCaverJsReact()
   const selectedCurrencyBalance = useCurrencyBalance(account ?? undefined, currency ?? undefined)
-  const [isMaxKlayNoti, setIsMaxKlayNoti] = useState<boolean>(false);
-  const [balance, setBalance] = useState<string>('');
+  const [isMaxKlayNoti, setIsMaxKlayNoti] = useState<boolean>(false)
+  const [balance, setBalance] = useState<string>('')
 
   const [onPresentCurrencySearchModal] = useModal(
-  <CurrencySearchModal
-    onCurrencySelect={onCurrencySelect}
-    selectedCurrency={currency}
-    otherSelectedCurrency={otherCurrency}
-  />, false);
+    <CurrencySearchModal
+      onCurrencySelect={onCurrencySelect}
+      selectedCurrency={currency}
+      otherSelectedCurrency={otherCurrency}
+    />,
+    false
+  )
 
-  const decimals = useMemo(() => 18, []);
+  const decimals = useMemo(() => 18, [])
   const overDp = useMemo(() => new BigNumber(value).decimalPlaces() > decimals, [value, decimals])
 
   const renderNoti = useCallback(() => {
-    if(isMaxKlayNoti) {
+    if (isMaxKlayNoti) {
       return (
         <Noti type={NotiType.ALERT} mt="12px">
           {t('Full payment of KLAY')}
         </Noti>
       )
     }
-    if(isInsufficientBalance){
+    if (isInsufficientBalance) {
       return (
         <Noti type={NotiType.ALERT} mt="12px">
           {t('Insufficient balance')}
         </Noti>
       )
     }
-    
-    if(overDp){
+
+    if (overDp) {
       return (
         <Noti type={NotiType.ALERT} mt="12px">
           {t('The value entered is out of the valid range')}
         </Noti>
       )
     }
-    return null;
-  }, [isInsufficientBalance, isMaxKlayNoti, t, overDp]);
+    return null
+  }, [isInsufficientBalance, isMaxKlayNoti, t, overDp])
 
   useEffect(() => {
-    if(!hideBalance && !!currency && selectedCurrencyBalance){
-      setBalance(selectedCurrencyBalance?.toFixed(5));
-      return;
+    if (!hideBalance && !!currency && selectedCurrencyBalance) {
+      setBalance(selectedCurrencyBalance?.toFixed(5))
+      return
     }
-    setBalance('-');
+    setBalance('-')
   }, [hideBalance, currency, selectedCurrencyBalance])
 
-
   useEffect(() => {
-    if(currency?.symbol === 'KLAY'){
-      if(value >= balance) {
-        setIsMaxKlayNoti(true);
-        return;
+    if (currency?.symbol === 'KLAY') {
+      if (value >= balance) {
+        setIsMaxKlayNoti(true)
+        return
       }
     }
-    setIsMaxKlayNoti(false);
-  }, [value, balance, maxTokenAmount, currency?.symbol]);
+    setIsMaxKlayNoti(false)
+  }, [value, balance, maxTokenAmount, currency?.symbol])
 
   return (
     <>
       <Box id={id} mb="12px">
-        <InputBox>
+        <Flex justifyContent="space-between">
           {!hideInput && (
-            <Flex
-              flexDirection="row"
-              flex="1 1 0"
-              pr="20px"
-            >
+            <Flex flexDirection="row" flex="1 1 0" pr="20px">
               <Flex flexDirection="column" flex="1" position="relative">
                 <Flex mb="4px">
-                  <Text textStyle="R_14R" color={ColorStyles.DEEPGREY} mr="4px" >
+                  <Text textStyle="R_14R" color={ColorStyles.DEEPGREY} mr="4px">
                     {t('Balance')}
                   </Text>
                   <Text textStyle="R_14B" color={ColorStyles.DEEPGREY}>
                     {balance}
                   </Text>
                 </Flex>
-                <NumericalInput
-                  value={value}
-                  onUserInput={(val) => onUserInput(val)}
-                />
-                {(account && currency && onQuarter && onHalf && onMax) && 
+                <NumericalInput value={value} onUserInput={(val) => onUserInput(val)} />
+                {account && currency && onQuarter && onHalf && onMax && (
                   <>
                     <Flex mt="8px">
                       <AnountButton onClick={onQuarter} mr="6px">
@@ -287,13 +259,11 @@ export default function CurrencyInputPanel({
                       <AnountButton onClick={onHalf} mr="6px">
                         {t('50%')}
                       </AnountButton>
-                      <AnountButton onClick={onMax}>
-                        {t('MAX')}
-                      </AnountButton>
+                      <AnountButton onClick={onMax}>{t('MAX')}</AnountButton>
                     </Flex>
                     {renderNoti()}
                   </>
-                }
+                )}
               </Flex>
             </Flex>
           )}
@@ -303,62 +273,43 @@ export default function CurrencyInputPanel({
             className="open-currency-select-button"
             onClick={() => {
               if (!disableCurrencySelect) {
-                onPresentCurrencySearchModal();
+                onPresentCurrencySearchModal()
               }
             }}
           >
             <Flex>
-              <Flex alignItems="center" height={isMobile ? "32px" : "40px"} mr="6px">
-                <Flex>
-                  {!disableCurrencySelect && <SmallDownIcon />}
-                </Flex>
+              <Flex alignItems="center" height={isMobile ? '32px' : '40px'} mr="6px">
+                <Flex>{!disableCurrencySelect && <SmallDownIcon />}</Flex>
               </Flex>
               <Flex flexDirection="column" alignItems="center">
                 <Flex mb="5px">
-                  {pair && 
-                    <Lp 
-                      lpSymbols={[pair.token0?.symbol, pair.token1?.symbol]} size="16px"
-                    />
-                  }
-                  {!pair && currency && 
-                    <Coin 
-                      symbol={currency?.symbol}
-                      size={isMobile ? "32px" : "40px"}
-                    />
-                  }
-                  {!pair && !currency && 
-                    <Coin 
-                      symbol="UNSELECT"
-                      size={isMobile ? "32px" : "40px"}
-                    />
-                  }
+                  {pair && <Lp lpSymbols={[pair.token0?.symbol, pair.token1?.symbol]} size="16px" />}
+                  {!pair && currency && <Coin symbol={currency?.symbol} size={isMobile ? '32px' : '40px'} />}
+                  {!pair && !currency && <Coin symbol="UNSELECT" size={isMobile ? '32px' : '40px'} />}
                 </Flex>
                 {pair && (
-                  <Text textStyle={isMobile ? "R_12B" : "R_14B"} color={ColorStyles.BLACK}>
+                  <Text textStyle={isMobile ? 'R_12B' : 'R_14B'} color={ColorStyles.BLACK}>
                     {pair?.token0.symbol}:{pair?.token1.symbol}
                   </Text>
                 )}
                 {!pair && (
-                  <Text
-                    textStyle="R_14B"
-                    color={ColorStyles.BLACK}
-                  >
+                  <Text textStyle="R_14B" color={ColorStyles.BLACK}>
                     {(currency && currency.symbol && currency.symbol.length > 20
                       ? `${currency.symbol.slice(0, 4)}...${currency.symbol.slice(
                           currency.symbol.length - 5,
                           currency.symbol.length
                         )}`
-                      : currency?.symbol) || 
-                        <Text textStyle="R_14B" color={ColorStyles.BLACK}>
-                          {t('Token')}
-                        </Text>
-                  }
+                      : currency?.symbol) || (
+                      <Text textStyle="R_14B" color={ColorStyles.BLACK}>
+                        {t('Token')}
+                      </Text>
+                    )}
                   </Text>
                 )}
               </Flex>
             </Flex>
           </CurrencySelect>
-        </InputBox>
+        </Flex>
       </Box>
     </>
   )
