d17a5b471b4c0bb7f4c4c68bce2fc4a39fa36f7a
GeonHyeok-Lee <geonhyeoklee.kr@gmail.com>
2021-12-29 20:13:05 +0900

    feat: Change style on SwapRoute [GZ2021-422]

--- src/components/SwapComponent/SwapRoute.tsx
+++ src/components/SwapComponent/SwapRoute.tsx
@@ -1,5 +1,5 @@
 import { Trade } from 'definixswap-sdk'
-import React, { Fragment, memo } from 'react'
+import React, { memo } from 'react'
 import { Text, Flex, ArrowDoubleArrowIcon, ColorStyles, Coin } from '@fingerlabs/definixswap-uikit-v2'
 import { useTranslation } from 'react-i18next'
 
@@ -18,16 +18,30 @@ export default memo(function SwapRoute({ trade, isMobile, isPriceImpactCaution }
       {!isPriceImpactCaution && trade.route.path.map((token, i, path) => {
         const isLastItem: boolean = i === path.length - 1
         return (
-          // eslint-disable-next-line react/no-array-index-key
-          <Fragment key={i}>
-            <Flex alignItems="center" mr="10px" mb="6px">
-              <Coin size={isMobile ? "20px" : "22px"} symbol={token?.symbol} />
-              <Text textStyle="R_14M" color={ColorStyles.DEEPGREY} ml="9px">
-                {token.symbol}
-              </Text>
-              {!isLastItem && <Flex ml="14px"><ArrowDoubleArrowIcon/></Flex>}
-            </Flex>
-          </Fragment>
+          <Flex 
+            alignItems="center"
+            key={token?.symbol}
+            mr={isLastItem ? "0px" : "10px"}
+            mb={isMobile ? "8px" : "0px"}
+          >
+            <Coin
+              size={isMobile ? "20px" : "22px"}
+              symbol={token?.symbol}
+              mr={isMobile ? "9px" : "6px"}
+            />
+            <Text
+              textStyle="R_14M"
+              color={ColorStyles.DEEPGREY}
+              mr={isLastItem ? "0px" : isMobile ? "14px" : "10px"}
+            >
+              {token.symbol}
+            </Text>
+            {!isLastItem && (
+              <Flex>
+                <ArrowDoubleArrowIcon />
+              </Flex>
+            )}
+          </Flex>
         )
       })}
       {isPriceImpactCaution && <Text textStyle="R_14M" color={ColorStyles.DEEPGREY}>{t('There are no routes.')}</Text>}
